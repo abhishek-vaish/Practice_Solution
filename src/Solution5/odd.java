2609ac88eb53fb2f3df3bdef4ba41acf911e16d9
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:11:51 -0700

    Create scanner object and take user input

--- src/Solution5/odd.java
+++ src/Solution5/odd.java
@@ -1,7 +1,11 @@
 package Solution5;
 
+import java.util.Scanner;
+
 public class odd {
     public static void main(String[] args){
         int num;
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
 
     }
