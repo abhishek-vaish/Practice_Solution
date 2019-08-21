203720cc0fe51cd023dea8a5a028cca5939d5a1e
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:20:37 -0700

    Create scanner object and declare required variables

--- src/Solution7/Solution7.java
+++ src/Solution7/Solution7.java
@@ -1,8 +1,13 @@
 package Solution7;
 
+import java.util.Scanner;
+
 public class Solution7 {
     public static void main(String[] args) {
         char value;
+        Scanner scan = new Scanner(System.in);
+        value = scan.next().charAt(0);
+        int value1 = value;
 
     }
 }
