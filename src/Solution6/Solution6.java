996155aae44ec0e7673c9933d07171ac0d242bee
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:14:41 -0700

    Create scanner object and take input from user

--- src/Solution6/Solution6.java
+++ src/Solution6/Solution6.java
@@ -1,8 +1,15 @@
 package Solution6;
 
+import java.util.Scanner;
+
 public class Solution6 {
     public static void main(String args[]){
         char str1;
         char str2;
+        Scanner scan = new Scanner(System.in);
+        str1 = scan.next().charAt(0);
+        str2 = scan.next().charAt(0);
+        int value = str1;
+        int value1 = str2;
 
 }
