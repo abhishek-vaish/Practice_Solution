bb432b3fb0ee7509234487130cbb10ace1aadff0
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 06:57:53 -0700

    Create object and take user input

--- src/practice/Solution1.java
+++ src/practice/Solution1.java
@@ -1,9 +1,15 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Solution1 {
     public static String mess;
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        mess = scan.nextLine();
 
-}
+
+    }
 
 }
