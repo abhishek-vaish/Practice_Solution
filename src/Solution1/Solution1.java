87ccda67b6d709afabe8662eb551fe4b9bac9905
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:01:33 -0700

    Declare variables

--- src/Solution1/Solution1.java
+++ src/Solution1/Solution1.java
@@ -1,4 +1,4 @@
-package practice;
+package Solution1;
 
 import java.util.Scanner;
 
---
+++ src/Solution2/Solution2.java
@@ -0,0 +1,10 @@
+package Solution2;
+
+public class Solution2 {
+    public static void main(String[] args){
+        String str1;
+        String str2;
+
+    }
+}
+
