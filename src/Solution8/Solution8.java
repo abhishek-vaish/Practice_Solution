ea414a6b0416216effe31f31c5ab4e5b93a266bd
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:30:52 -0700

    Create scanner object and take user input

--- src/Solution8/Solution8.java
+++ src/Solution8/Solution8.java
@@ -1,6 +1,14 @@
 package Solution8;
 
+import java.util.Scanner;
+
 public class Solution8 {
-    String gender;
-    int age;
+    public static void main(String[] args) {
+        String gender;
+        int age;
+        Scanner scan = new Scanner(System.in);
+        gender = scan.nextLine();
+        age = scan.NextInt();
+    }
+
 }
