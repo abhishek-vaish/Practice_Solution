21cc067008aaa7a1493cef34b307c32785ff9ec7
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:09:16 -0700

    Create scanner object and take user input

--- src/Solution4/Solution4.java
+++ src/Solution4/Solution4.java
@@ -1,7 +1,11 @@
 package Solution4;
 
+import java.util.Scanner;
+
 public class Solution4 {
     public static void main(String[] args){
         int num;
+        Scanner scan = new Scanner(System.in);
+        num = scan.nextInt();
 
     }
