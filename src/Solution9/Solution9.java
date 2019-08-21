e647b1b0135139c44fbb8d18001cebe74a1dc249
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 10:34:26 -0700

    Create required condition and print output

--- src/Solution9/Solution9.java
+++ src/Solution9/Solution9.java
@@ -6,7 +6,11 @@ public class Solution9 {
         char value;
         Scanner scan = new Scanner(System.in);
         value = scan.next().charAt(0);
-
+        int value1 = value;
+        if (value1 >= 97 && value1 <= 122)
+            System.out.println(Character.toUpperCase(value));
+        else if (value >= 65 && value1 <= 90)
+            System.out.println(Character.toLowerCase(value));
 
     }
 
