3b5360470eaff374a4f40dd79945cb34923e9e0e
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:24:39 -0700

    Put required condition and print output

--- src/Solution7/Solution7.java
+++ src/Solution7/Solution7.java
@@ -8,6 +8,11 @@ public class Solution7 {
         Scanner scan = new Scanner(System.in);
         value = scan.next().charAt(0);
         int value1 = value;
-
+        if (value1 >= 97 && value1 <= 122 || value1 >= 65 && value1 <= 90)
+            System.out.println("Alphabet");
+        else if (value1 >= 48 && value1 <= 57)
+            System.out.println("Digits");
+        else
+            System.out.println("Special Character");
     }
 }
