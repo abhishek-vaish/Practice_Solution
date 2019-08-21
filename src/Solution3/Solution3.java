bd4a89e2b3737de59ec78f61a5b25a8174ec304c
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:07:01 -0700

    Print required output

--- src/Solution3/Solution3.java
+++ src/Solution3/Solution3.java
@@ -9,6 +9,11 @@ public class Solution3 {
         Scanner scan = new Scanner(System.in);
         num1 = scan.nextInt();
         num2 = scan.nextInt();
-
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+    }
 
     }
