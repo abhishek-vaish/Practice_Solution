ff8d36aafc31245a47abe8a1c2cec4fb7a76ebdf
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:34:03 -0700

    Put required condition and print output

--- src/Solution8/Solution8.java
+++ src/Solution8/Solution8.java
@@ -9,6 +9,14 @@ public class Solution8 {
         Scanner scan = new Scanner(System.in);
         gender = scan.nextLine();
         age = scan.nextInt();
+        if (gender == "female" && age >= 1 && age <= 58)
+            System.out.println("8.2%");
+        else if (gender == "female" && age >= 59 && age <= 100)
+            System.out.println("9.2%");
+        else if (gender == "male" && age >= 1 && age <= 58)
+            System.out.println("8.4%");
+        else if (gender == "male" && age >= 59 && age <= 100)
+            System.out.println("10.5%");
     }
 
 }
