3c6fd0668766bd01e2f2b2bafc623afe704b016a
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-21 07:09:49 -0700

    Put required condition and print output

--- src/Solution4/Solution4.java
+++ src/Solution4/Solution4.java
@@ -7,5 +7,11 @@ public class Solution4 {
         int num;
         Scanner scan = new Scanner(System.in);
         num = scan.nextInt();
-
+        if (num < 0) {
+            System.out.println("Negative");
+        } else if (num > 0)
+            System.out.println("Positive");
+        else
+            System.out.println("Zero");
     }
+}
