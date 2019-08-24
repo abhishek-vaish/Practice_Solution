b58527a2896d46a69fdafdae6061541367caa3d8
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-23 21:35:26 -0700

    Put required condition and print output.

--- src/Solution13/Solution13.java
+++ src/Solution13/Solution13.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class Solution13 {
     public static void main(String[] args) {
         int value;
+        int sum = 0;
         Scanner scan = new Scanner(System.in);
         value = scan.nextInt();
-
+        int result;
+        while (value > 0) {
+            result = value;
+            result = result % 10;
+            value = value / 10;
+            sum = sum + result;
+        }
+        System.out.println(sum);
     }
 }
