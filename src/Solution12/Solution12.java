2265e687e795bcd04c694d02de0aff78a15ba36b
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-23 21:57:31 -0700

    Create scanner object and take user input

--- src/Solution12/Solution12.java
+++ src/Solution12/Solution12.java
@@ -1,8 +1,14 @@
 package Solution12;
 
+import java.util.Scanner;
+
 public class Solution12 {
     public static void main(String[] args) {
         int value;
+        Scanner scan = new Scanner(System.in);
+        value = scan.nextInt();
 
+
+    }
     }
 }
