a72e3f5070102848d267e5ef50ad48931afd488e
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-23 21:58:07 -0700

    Put required commands and print output

--- src/Solution12/Solution12.java
+++ src/Solution12/Solution12.java
@@ -7,7 +7,14 @@ public class Solution12 {
         int value;
         Scanner scan = new Scanner(System.in);
         value = scan.nextInt();
-
+        for (int i = 2; i < value; i++) {
+            if (value % i == 0) {
+                System.out.println(value + " is not a Prime number.");
+                break;
+            } else {
+                System.out.println(value + " is a Prime number.");
+                break;
+            }
 
     }
     }
