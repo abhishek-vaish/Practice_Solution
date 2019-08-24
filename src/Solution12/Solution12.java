a45fc0ef848da54693b9835b1afab8422023c742
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2019-08-23 21:57:10 -0700

    Create variables

--- src/Solution12/Solution12.java
+++ src/Solution12/Solution12.java
@@ -1,22 +1,8 @@
 package Solution12;
 
-import java.util.Scanner;
-
 public class Solution12 {
     public static void main(String[] args) {
         int value;
-        Scanner scan = new Scanner(System.in);
-        value = scan.nextInt();
-        for (int i = 2; i < value; i++) {
-            if (value % i == 0) {
-                System.out.println(value + " is not a Prime number.");
-                break;
-            } else {
-                System.out.println(value + " is a Prime number.");
-                break;
-            }
 
-
-        }
     }
 }
